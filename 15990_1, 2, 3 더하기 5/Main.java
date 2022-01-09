f1711377ffbdfdc7251296d497725d16a348d5ee
jungsoonsung <jungss38@gmail.com>
2022-01-09 22:01:58 +0900

    test

---
+++ 15990_1, 2, 3 더하기 5/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fr = new FileReader("input.txt");
+        BufferedReader br = new BufferedReader(fr);
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
