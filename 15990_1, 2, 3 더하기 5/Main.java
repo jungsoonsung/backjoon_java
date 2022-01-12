ca026281aa262b522e252520cd08db2f662c4e1a
jungsoonsung <jungss38@gmail.com>
2022-01-12 10:26:08 +0900

    add 1085454

--- 15990_1, 2, 3 더하기 5/Main.java
+++ 15990_1, 2, 3 더하기 5/Main.java
@@ -1,14 +1,31 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+
+    static int DIV = 1000000009;
+    static int MAX = 100000;
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         FileReader fr = new FileReader("input.txt");
         BufferedReader br = new BufferedReader(fr);
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        int[][] DP = new int[MAX+1][4];
+        DP[1][1] = DP[2][2] = DP[3][3] = DP[3][1] = DP[3][2] = 1;
+        for(int n=4; n<MAX+1; n++){
+            DP[n][1] = DP[n-1][2] + DP[n-1][3];
+            DP[n][2] = DP[n-2][1] + DP[n-2][3];
+            DP[n][3] = DP[n-3][1] + DP[n-3][2];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int TC = Integer.parseInt(br.readLine());
+        for(int t=0; t<TC; t++){
+            int n = Integer.parseInt(br.readLine());
+            int res = (DP[n][1] + DP[n][2] + DP[n][3]) % DIV;
+            sb.append(res).append("\n");
+        }
+        System.out.println(sb.toString());
+
+        br.close();
     }
 }
